e61e77fee407857269404944d3c5edd289c876f8
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-07-29 21:06:42 +0300

    feat: create container component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,21 +4,25 @@ import styled from "styled-components";
 import {Logo} from "components/logo/Logo";
 import {Menu} from "components/menu/Menu";
 import {SocialsIconList} from "components/socials/Socials";
+import {Container} from "components/Container";
+import {FlexWrapper} from "components/FlexWrapper";
 
 const items = ["Home", "About", "Tech Stack", "Projects", "Contact"];
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
-            <SocialsIconList/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                    <SocialsIconList/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
   background-color: #c6f5b0;
-  display: flex;
-  justify-content: space-between;
 `
